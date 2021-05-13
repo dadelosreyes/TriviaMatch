3dbd3428b5250a09676419d5299a3124e19cd35b
Dave Delos Reyes <DELOSREYES.DAVE@UE.EDU.PH>
2021-05-14 02:10:50 +0800

    added png files and did some back end work

--- app/src/main/java/com/example/triviamatch/MainActivity.java
+++ app/src/main/java/com/example/triviamatch/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.triviamatch;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    Animation scaleUp, scaleDown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,12 +19,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button btnPlay= findViewById(R.id.play);
         Button btnInstructions= findViewById(R.id.instructions);
+        scaleUp = AnimationUtils.loadAnimation(this,R.anim.up_scale);
+        scaleDown = AnimationUtils.loadAnimation(this,R.anim.down_scale);
+
+        btnPlay.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN)
+                {
+                    btnPlay.startAnimation(scaleUp);
+                }
+                else if(event.getAction() == MotionEvent.ACTION_UP)
+                {
+                    btnPlay.startAnimation(scaleDown);
+                }
+                return false;
+            }
+        });
+        btnInstructions.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN)
+                {
+                    btnInstructions.startAnimation(scaleUp);
+                }
+                else if(event.getAction() == MotionEvent.ACTION_UP)
+                {
+                    btnInstructions.startAnimation(scaleDown);
+                }
+                return false;
+            }
+        });
 
         btnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, MainGame.class);
-                startActivity(intent);
+                //startActivity(intent);
             }
         });
         btnInstructions.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/triviamatch/MainGame.java
+++ app/src/main/java/com/example/triviamatch/MainGame.java
@@ -1,13 +1,36 @@
 package com.example.triviamatch;
 
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainGame extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainGame extends AppCompatActivity implements View.OnClickListener {
+    private static final int[] button_Ids = {
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_game);
+
+        List<Button> buttons = new ArrayList<Button>();
+        // or slightly better
+        // buttons = new ArrayList<Button>(BUTTON_IDS.length);
+        for(int id : button_Ids) {
+            Button button = (Button)findViewById(id);
+            button.setOnClickListener(this); // maybe
+            buttons.add(button);
+        }
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
